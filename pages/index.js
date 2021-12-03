a1a0dd600612120bc47adc710eb74657c18aecb7
ekankam <eb.kankam26@gmail.com>
2021-12-02 22:48:54 -0600

    Added a medium card component for the live anywhere section. Made a fetch request for data. Passed props through component and applied stylings.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 
-export default function Home({ res: exploreData }) {
-    console.log(exploreData)
+export default function Home({ res: exploreData, cardRes: cardsData }) {
     return (
         <div className="">
             <Head>
@@ -32,6 +32,16 @@ export default function Home({ res: exploreData }) {
                         ))}
                     </div>
                 </section>
+                <section>
+                    <h2 className="text-4xl font-semibold py-8">
+                        Live Anywhere
+                    </h2>
+                    <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
+                        {cardsData?.map(({ img, title }) => (
+                            <MediumCard key={img} img={img} title={title} />
+                        ))}
+                    </div>
+                </section>
             </main>
         </div>
     )
@@ -40,9 +50,13 @@ export default function Home({ res: exploreData }) {
 export async function getStaticProps() {
     const data = await fetch('https://links.papareact.com/pyp')
     const res = await data.json()
+
+    const cardData = await fetch('https://links.papareact.com/zp1')
+    const cardRes = await cardData.json()
     return {
         props: {
             res,
+            cardRes,
         },
     }
 }
