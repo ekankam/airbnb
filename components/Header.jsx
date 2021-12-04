40d8c3d8e97c70edf7a324d2be000952de2fa1f8
ekankam <eb.kankam26@gmail.com>
2021-12-03 23:04:21 -0600

    Added a router and searchHandler function to get the user input.  Added router to the Search components. Formatted the dates.

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,12 +10,15 @@ import {
 import 'react-date-range/dist/styles.css' // main style file
 import 'react-date-range/dist/theme/default.css' // theme css file
 import { DateRangePicker } from 'react-date-range'
+import { useRouter } from 'next/dist/client/router'
 
 const Header = () => {
     const [searchInput, setSearchInput] = useState('')
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
     const [numberOfGuests, setNumberOfGuests] = useState(1)
+
+    const router = useRouter()
 
     const handleSelect = (ranges) => {
         setStartDate(ranges.selection.startDate)
@@ -28,10 +31,25 @@ const Header = () => {
         key: 'selection',
     }
 
+    const searchHandler = () => {
+        router.push({
+            pathname: '/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                numberOfGuests,
+            },
+        })
+    }
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/* Left */}
-            <div className="relative flex items-center h-10 cursor-pointer">
+            <div
+                className="relative flex items-center h-10 cursor-pointer"
+                onClick={() => router.push('/')}
+            >
                 <Image
                     src="https://links.papareact.com/qd3"
                     layout="fill"
@@ -90,7 +108,10 @@ const Header = () => {
                         >
                             Cancel
                         </button>
-                        <button className="flex-grow text-red-400">
+                        <button
+                            className="flex-grow text-red-400"
+                            onClick={searchHandler}
+                        >
                             Search
                         </button>
                     </div>
--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,26 @@
+import { useRouter } from 'next/dist/client/router'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { format } from 'date-fns'
 
 const Search = () => {
+    const router = useRouter()
+    const { location, startDate, endDate, numberOfGuests } = router.query
+
+    const formattedStartDate = format(new Date(startDate), 'dd MMMM yy')
+    const formattedEndDate = format(new Date(endDate), 'dd MMMM yy')
+    const range = `${formattedStartDate} - ${formattedEndDate}`
     return (
         <div>
             <Header />
             <main className="flex">
                 <section className="flex-grow pt-14 px-6">
-                    <p className="text-xs">300+ Stays for 5 number of guests</p>
+                    <p className="text-xs">
+                        300+ Stays - {range} - for {numberOfGuests} guests
+                    </p>
 
                     <h1 className="text-3xl font-semibold mt-2 mb-6">
-                        Stays in Mars
+                        Stays in {location}
                     </h1>
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
