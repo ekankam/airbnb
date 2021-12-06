d7856e16bf2ec63737f4aa00e752aa86455237c4
ekankam <eb.kankam26@gmail.com>
2021-12-06 11:08:38 -0600

    Added a map component

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import { format } from 'date-fns'
 import InfoCard from '../components/InfoCard'
+import Map from '../components/Map'
 
 const Search = ({ searchResults }) => {
     const router = useRouter()
@@ -56,6 +57,9 @@ const Search = ({ searchResults }) => {
                             )
                         )}
                     </div>
+                </section>
+                <section className="">
+                    <Map />
                 </section>
             </main>
             <Footer />
